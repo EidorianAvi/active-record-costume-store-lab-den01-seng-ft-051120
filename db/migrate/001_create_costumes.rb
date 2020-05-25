2da88ea9581c3723e57c9d1c9953fab38fc0863f
Rene Avila <reneavila1993@gmail.com>
2020-05-25 20:06:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -17,7 +17,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.integer :size
       t.string :image_url
-      t.timestamp
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
